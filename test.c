47db07f1102b66c208aee1027ad98e1e75d9eb94
Likeness3884 <amirhossienbnc124@gmail.com>
2023-05-07 14:10:41 -0400

    print 0 to 3

--- test.c
+++ test.c
@@ -7,6 +7,10 @@ int main()
 	printf("you entered %d\n", number);
 	printf("Done :D\n");
 	printf("literally done :DDD\n");
+	for (int i = 0; i < 4; i++)
+	{
+		printf("%d\n", i);
+	}
 
 	return 0;
 }
